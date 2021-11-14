24cca36fd998442fa8afddb83b0dc06b4796edc2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2021-11-14 18:54:05 +0900

    wip

---
+++ crates/swc/tests/fixture/issue-2670/1/input/index.ts
@@ -0,0 +1,10 @@
+export namespace TEST {
+  export const x0 = 0;
+  export const x1 = x0;
+
+  const y0 = 0;
+  export const y1 = y0;
+
+  export const z0 = 0;
+  export const z1 = TEST.z0;
+}
---
+++ crates/swc/tests/fixture/issue-2670/1/output/index.ts
@@ -0,0 +1,9 @@
+var TEST1;
+export { TEST1 as TEST };
+(function(TEST) {
+    TEST.x0 = 0;
+    TEST.x1 = x0;
+    var y0 = 0;
+    TEST.y1 = y0;
+})(TEST1 || (TEST1 = {
+}));
